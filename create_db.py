6f9e775d31b72b44d5116ed23878ca8a55764f44
setiyawan12 <yayangsetiyawan0@gmail.com>
2019-12-20 08:34:15 +0700

    UAS

---
+++ create_db.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+
+    host="localhost",
+    user="root",
+    passwd=""
+)
+
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE toko_mainan")
+
+print("Database berasil di buat")
